4c8fa17811b6dde814061129ee789b3a851e4e76
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-04-19 16:32:41 +0530

    crud

--- Crud.java
+++ Crud.java
@@ -43,6 +43,90 @@ public class Crud {
                 }
                 break;
 
+            case 2:
+                System.out.println("option 2 -- insert a record");
+                try {
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+                    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "root", "");
+                    System.out.println("Connection established");
+                    Statement stmt = con.createStatement();
+                    System.out.println("Enter id: ");
+                    int id = myObj.nextInt();
+                    System.out.println("Enter name: ");
+                    String name = myObj.next();
+                    System.out.println("Enter age: ");
+                    int age = myObj.nextInt();
+                    String Sql = "INSERT INTO test  VALUES (" + id + ",'" + name + "'," + age + ")";
+                    stmt.executeUpdate(Sql);
+
+                    System.out.println("Record inserted successfully with id :" + id);
+                } catch (Exception e) {
+                    System.out.println("Error in inserting record" + e.getMessage());
+                }
+                break;
+
+            case 3:
+                System.out.println("option 3 -- delete a record");
+                try {
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+                    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "root", "");
+                    System.out.println("Connection established");
+                    Statement stmt = con.createStatement();
+                    System.out.println("Enter id: ");
+                    int id = myObj.nextInt();
+                    String Sql = "DELETE FROM test WHERE id=" + id;
+                    stmt.executeUpdate(Sql);
+
+                    System.out.println("Record deleted successfully with id :" + id);
+                } catch (Exception e) {
+                    System.out.println("Error in deleting record" + e.getMessage());
+                }
+                break;
+
+            case 4:
+                System.out.println("option 4 -- update a record");
+                try {
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+                    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "root", "");
+                    System.out.println("Connection established");
+                    Statement stmt = con.createStatement();
+                    System.out.println("Enter id: ");
+                    int id = myObj.nextInt();
+                    System.out.println("Enter name: ");
+                    String name = myObj.next();
+                    System.out.println("Enter age: ");
+                    int age = myObj.nextInt();
+                    String Sql = "UPDATE test SET name='" + name + "', age=" + age + " WHERE id=" + id;
+                    stmt.executeUpdate(Sql);
+
+                    System.out.println("Record updated successfully with id :" + id);
+                } catch (Exception e) {
+                    System.out.println("Error in updating record" + e.getMessage());
+                }
+                break;
+
+            case 5:
+                System.out.println("option 5 -- search a record");
+                try {
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+                    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "root", "");
+                    System.out.println("Connection established");
+                    Statement stmt = con.createStatement();
+                    System.out.println("Enter id: ");
+                    int id = myObj.nextInt();
+                    String Sql = "SELECT * FROM test WHERE id=" + id;
+                    ResultSet rs = stmt.executeQuery(Sql);
+
+                    while (rs.next()) {
+                        System.out.print("ID: " + rs.getInt("id"));
+                        System.out.print(", Name: " + rs.getString("name"));
+                        System.out.println(", Age: " + rs.getInt("age"));
+                    }
+                } catch (Exception e) {
+                    System.out.println("Error in searching record" + e.getMessage());
+                }
+                break;
+
             case 6:
                 System.out.println("option 1 -- create a table");
                 System.out.println("Enter table name: ");
@@ -59,6 +143,8 @@ public class Crud {
                 } catch (Exception e) {
                     System.out.println("Error in deleting table" + e.getMessage());
                 }
+                break;
+
             default:
                 System.out.println("Invalid choice");
                 break;
